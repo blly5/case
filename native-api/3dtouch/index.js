1b619227c22e314257263071ade21a0045ce2eb5
blue <574519075@qq.com>
2019-05-23 10:44:33 +0800

    [fix]

--- native-api/3dtouch/index.js
+++ native-api/3dtouch/index.js
@@ -6,17 +6,21 @@
     let body = document.querySelector('body');
 
     function touchLog( val ) {
-       
+        document.querySelector('body').innerHTML = '';
         let el = document.createElement('span');
         el.innerText = val;
         document.body.append( el );
     }
 
+
+    
     function forEach( arr, callback ) {
         console.log(arr);
         return Array.prototype.forEach.call(arr, callback);
         return forEach( callback );
     };
+    
+    console.log('done');
 
     body.addEventListener('touchstart', (e) => {
        let touchs = e.targetTouches;
