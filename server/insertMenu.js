a133657e31037a93812c3565d0da52de2485ca22
blue <574519075@qq.com>
2019-05-24 10:18:21 +0800

    [fix] **

--- server/insertMenu.js
+++ server/insertMenu.js
@@ -2,7 +2,7 @@
  * @Author: Blue
  * @Date: 2019-05-23 16:49:09
  * @Last Modified by: blue
- * @Last Modified time: 2019-05-23 18:36:37
+ * @Last Modified time: 2019-05-23 20:03:08
  */
 
 
@@ -15,38 +15,38 @@ const readline =  require('readline');
 
 
 async function getContent () {
-    try {
-        return fs.readFileSync(`${ path.resolve('../') }/index.json`, 'utf-8');
-    } catch( e ) {
-    console.log( e );
-    }
+    return await fs.readFileSync(`${ path.resolve('./') }/index.json`, 'utf-8');
 };
 
 async function getMessage() {
- 
     return new Promise((resolve, reject) => {
         let rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
           });
         let msg = '';
+        console.log( chalk.blue('请输入:') )
         rl.on('line', ( input ) => {
             msg = input;
             rl.close();
             resolve(msg);
         }); 
-    });
+    });   
+};
 
-}
+async function setContent( i ) {
+    return await fs.writeFileSync(`${ path.resolve('./') }/index.json`, i );
+};
 
 async function addContent( i ) {
     try {
-    
-    let content = JSON.parse( await getContent() || `{}` ) ; 
-    
-    if( JSON.stringify( content ) === '{}' ) {
-        console.log( await getMessage() );
-    }
+        
+        let content = JSON.parse( await getContent() ) ; 
+
+        let inputMsg = await getMessage();
+        let w = { ...content, inputMsg };
+        
+        await setContent( JSON.stringify( content ) );     
     } catch( e ) {
         console.log( chalk.red( e ) );
     }
