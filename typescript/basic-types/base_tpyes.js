61a70169063913a1b941507b8784350b3d016766
blue <574519075@qq.com>
2019-05-24 20:13:26 +0800

    [feat] TS init

---
+++ typescript/basic-types/base_tpyes.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Blue
+ * @Date: 2019-05-23 16:49:09
+ * @Last Modified by: blue
+ * @Last Modified time: 2019-05-24 15:08:39
+ */
+function parint(lable) {
+    console.log(lable.add);
+}
+;
---
+++ typescript/basic-types/base_tpyes.ts
@@ -0,0 +1,7 @@
+/*
+ * @Author: Blue
+ * @Date: 2019-05-23 16:49:09
+ * @Last Modified by: blue
+ * @Last Modified time: 2019-05-24 15:08:39
+ */
+
