98b698e81eb64f608246f5e2981498a39673a9fb
blue <574519075@qq.com>
2019-05-25 17:00:41 +0800

    [feat] TS function

--- typescript/basic-types/base_tpyes.js
+++ typescript/basic-types/base_tpyes.js
@@ -4,7 +4,20 @@
  * @Last Modified by: blue
  * @Last Modified time: 2019-05-24 15:08:39
  */
-function parint(lable) {
-    console.log(lable.add);
+var Site = 'CN';
+var Colors = 7;
+var List = [1, 2, 3, 4];
+var List2 = [1, 2, 3, 4];
+//tuple
+var List3 = ['1', 2, false];
+var Site1;
+(function (Site1) {
+    Site1[Site1["AN"] = 0] = "AN";
+    Site1[Site1["SH"] = 1] = "SH";
+    Site1[Site1["BJ"] = 2] = "BJ";
+})(Site1 || (Site1 = {}));
+;
+function Parint(a) {
+    console.log('done');
 }
 ;
--- typescript/basic-types/base_tpyes.ts
+++ typescript/basic-types/base_tpyes.ts
@@ -5,3 +5,25 @@
  * @Last Modified time: 2019-05-24 15:08:39
  */
 
+let Site: String = 'CN';
+
+let Colors: Number = 7;
+
+
+
+let List: Number[] = [1,2,3,4];
+
+
+let List2: Array<Number> = [1,2,3,4];
+
+//tuple
+
+let List3: [String, Number, Boolean] = ['1', 2, false]; 
+
+
+enum Site1 {'AN', 'SH', 'BJ' };
+
+function Parint( a ):void  {
+    console.log('done');
+};
+
