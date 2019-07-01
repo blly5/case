3bfaf5b58541eaf310aca236a090bafedad03eed
李宇 <574519075@qq.com>
2019-07-01 15:55:09 +0800

    chore(typescript) interface🔧

--- typescript/interface/interface.ts
+++ typescript/interface/interface.ts
@@ -2,7 +2,7 @@
  * @Author: Blue
  * @Date: 2019-05-23 16:49:09
  * @Last Modified by: Blue
- * @Last Modified time: 2019-06-29 18:27:47
+ * @Last Modified time: 2019-07-01 15:53:58
  */
 
  //必传接口
@@ -24,23 +24,66 @@ function parint2( type:parint2Value ) {
 };
 
 
+
 //optionbags mode
 
-function parint4() {
+interface parint4Value {
+    color: String,
+    num  : Number
+}
 
+function parint4( type: parint4Value ): { color:String } {
+    let { color, num } = type;
+    return { color };
 }
 
 
+interface parint5Value {
+    color?: String,
+    num   : Number,
+    tag   : String
+}
+
+// function parint5(config: parint5Value):{ color:String } {
+//     if(config.colo) {   //属性“colo”在类型“parint5Value”上不存在。你是否指的是“color”?
+//         return { color:config.color };
+//     }
+// }
+
+
+
+//readOnly
+
+
 //只读参数
- function parint3( type: {readonly color: number } ) {
+function parint3( type: {readonly color: number } ) {
     //  type.color = '213';
     // Cannot assign to 'color' because it is a read-only property.ts(2540)
     console.log(type);
  };
 
-
 //只读数组
-
 let arr: number[] = [5, 55, 555];
-const readOnlyArr: ReadonlyArray<any> = arr;
+let readOnlyArr: ReadonlyArray<any> = arr;
+
+//readOnly作为一个变量、可以使用const、属性可以使用readOnly;
+
+
+
+interface parint6Value {
+    color?: String,
+    Num  ?: Number
+};
+
+function parint5( config: parint6Value ) {
+    
+};
+
+let myParint =  parint5({colo:'12'}  as parint6Value );
+
+interface parint7Value {
+    color?: String,
+    Num  ?: Number
+};
+
 
