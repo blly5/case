7bbf91b9749314786651d205368a87ba51f7bf9f
李宇 <574519075@qq.com>
2019-07-18 11:43:57 +0800

    :white_check_mark: (fix) 路径定义
    webServer 添加定义路径

--- web-server/index.js
+++ web-server/index.js
@@ -2,7 +2,7 @@
  * @Author: Blue 
  * @Date: 2019-07-10 15:06:11 
  * @Last Modified by: Blue
- * @Last Modified time: 2019-07-13 16:51:28
+ * @Last Modified time: 2019-07-18 11:42:40
  */
 
 const express =         require('express');
@@ -10,17 +10,21 @@ const path =            require('path');
 const chalk =           require('chalk');
 const execa =           require('execa');
 const process =         require('process');
+const rqPort =          require('requires-port');
+
+
 const opt = function (res, path, stat) {
   res.set('Access-Control-Allow-Origin', '*')
 };
 
-
 let app =           express();
 let defalutPort =   8080;
-let _path =         !!process.argv.splice(2)[0] ? process.argv.splice(2)[0] : './';
+
+let _path = JSON.parse(process.env.npm_config_argv).remain[0] ? JSON.parse(process.env.npm_config_argv).remain[0] : './';
+
+console.log(`process.argv.splice`, process );
 
 app.use( '/', express.static( _path , { setHeaders: opt }));
 
 app.listen( defalutPort );
-console.log(chalk['green'](`\n  127.0.0.1:${ defalutPort } - path:${ _path } \n`));
-
+console.log(chalk['green'](`\n  http://127.0.0.1:${ defalutPort } / path:${ _path } \n`));
