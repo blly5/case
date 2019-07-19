082cab32dc7f88066510504fe06db14b2be57d3a
李宇 <574519075@qq.com>
2019-07-19 17:38:23 +0800

    :recycle: (chore) decorator

---
+++ javascript/es6/decorator/index.js
@@ -0,0 +1,98 @@
+/*
+ * @Author: Blue
+ * @Date: 2019-07-12 16:58:48
+ * @Last Modified by: Blue
+ * @Last Modified time: 2019-07-19 17:37:26
+ */
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+//1. decorator
+var A = /** @class */ (function () {
+    function A() {
+    }
+    A.isTest = function () {
+        console.log("is A");
+    };
+    A = __decorate([
+        B
+    ], A);
+    return A;
+}());
+;
+function B(target) {
+    target.isTest = true;
+    console.log(target);
+}
+;
+console.log(A.isTest);
+//true
+// function C(target) {
+//    target.test = true;
+// }
+// function D() {}
+// D = C(D) || D;
+// console.log(D.test); true
+//2. 多个参数
+function E(isTest) {
+    return function (target) {
+        target.isTest = isTest;
+        target.testMsg = function (testMsg) {
+            alert(testMsg);
+        };
+    };
+}
+;
+var F = /** @class */ (function () {
+    function F() {
+    }
+    F.isTest = false;
+    F.testMsg = {};
+    F = __decorate([
+        E(true)
+    ], F);
+    return F;
+}());
+;
+console.log(F.isTest);
+// F.testMsg('halo');   true, alert('halo')
+//3. prototype
+var G = /** @class */ (function () {
+    function G() {
+    }
+    G.isTest = false;
+    G = __decorate([
+        H
+    ], G);
+    return G;
+}());
+;
+function H(target) {
+    target.prototype.isTest = 'halo';
+}
+;
+var I = new G();
+// console.log(I.isTest);  halo
+// 4. 属性修饰
+var J = /** @class */ (function () {
+    function J() {
+    }
+    J.prototype.name = function () {
+        return "";
+    };
+    __decorate([
+        K
+    ], J.prototype, "name");
+    return J;
+}());
+;
+function K(target, name, descriptor) {
+    descriptor.enumerable = false;
+    return descriptor;
+}
+;
+var s = new J();
+console.log(s);
--- javascript/es6/decorator/index.ts
+++ javascript/es6/decorator/index.ts
@@ -3,8 +3,87 @@
  * @Author: Blue 
  * @Date: 2019-07-12 16:58:48 
  * @Last Modified by: Blue
- * @Last Modified time: 2019-07-13 15:50:04
+ * @Last Modified time: 2019-07-19 17:37:26
  */
+
+
+ //1. decorator
+ @B
+ class A {
+   static isTest() {
+      console.log(`is A`);
+   }
+ };
+
+ function B(target) {
+    target.isTest = true;
+    console.log(target);
+ };
+
+console.log(A.isTest);
+
+//true
+
+// function C(target) {
+//    target.test = true;
+// }
+
+// function D() {}
+// D = C(D) || D;
+
+// console.log(D.test); true
+
+
+//2. 多个参数
+
+function E( isTest ) {
+   return function( target ) {
+      target.isTest = isTest;
+      target.testMsg = function(testMsg) {
+         alert(testMsg);
+      }
+   }
+};
+@E(true)
+class F {
+   static isTest = false;
+   static testMsg = {};
+};
+console.log(F.isTest);
+
+// F.testMsg('halo');   true, alert('halo')
+
+
+//3. prototype
+
+@H
+class G {
+   static isTest = false;   
+};
+function H( target ) {
+   target.prototype.isTest = 'halo';
+};
+let I = new G();
+
+// console.log(I.isTest);  halo
+
+
+// 4. 属性修饰
+
+class J {
+   @K
+   name() {
+      return ``
+   }
+};
+
+function K(target, name, descriptor ) {
+   descriptor.enumerable = false;
+   return descriptor;
+};
+let s = new J();
+console.log(s);
+
 
 
 
