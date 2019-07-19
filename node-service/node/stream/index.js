aafa85733cdb5905818ee217742fcbde2c054412
李宇 <574519075@qq.com>
2019-07-19 18:19:28 +0800

    :rotating_light: (fix) 修改计划文件

--- node-service/node/stream/index.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var file = fs.createWriteStream('./1.txt');
-for (var i = 0; i < 1e6; i++) {
-    file.write("location:" + i);
-}
-;
-file.end();
--- node-service/node/stream/index.ts
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const file = fs.createWriteStream('./1.txt');
-
-for(let i =0; i < 1e6; i++) {
-    file.write(`location:${i}`);
-};
-file.end();
