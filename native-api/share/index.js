cc68a03908909b076eb7ba542d56421850d8bf21
L1Yuu <i@l1yu.com>
2019-09-05 16:43:08 +0800

    :fire: 删除了些多余文件

--- native-api/share/index.js
+++ native-api/share/index.js
@@ -2,16 +2,15 @@
  * @Author: Blue 
  * @Date: 2019-09-05 15:30:38 
  * @Last Modified by: Blue
- * @Last Modified time: 2019-09-05 16:22:49
+ * @Last Modified time: 2019-09-05 16:42:44
  */
 
- 
+ //@link: https://www.infoq.cn/article/gLxax1LYuqLDYZ-I4eKK
 
  function pageShare() {
   let shareData = {
     text: `三年二班`,
     url: `https://www.l1yu.com`
-    
   };
   navigator.share(shareData);
  }
