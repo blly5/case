1438e5e3196489e0460b6021c3081146f4aee78d
L1Yuu <i@l1yu.com>
2019-12-05 11:32:44 +0800

    chore(webServer)

--- web-server/index.js
+++ web-server/index.js
@@ -2,16 +2,17 @@
  * @Author: Blue 
  * @Date: 2019-07-10 15:06:11 
  * @Last Modified by: Blue
- * @Last Modified time: 2019-09-10 14:50:42
+ * @Last Modified time: 2019-12-05 11:28:35
  */
 
-const express =         require('express');
-const path =            require('path');
-const chalk =           require('chalk');
-const execa =           require('execa');
-const process =         require('process');
-const portFinder =      require('portfinder');
-const app =             express();
+const express = require('express');
+const path = require('path');
+const chalk = require('chalk');
+const execa = require('execa');
+const process = require('process');
+const portFinder = require('portfinder');
+const app = express();
+const upload = require('./upload');
 
 let defalutPort;
 async function checkNetWorkPort() {
@@ -27,10 +28,13 @@ const headerOption = function (res, path, stat) {
 
 let staticPath = JSON.parse(process.env.npm_config_argv).remain[0] ? JSON.parse(process.env.npm_config_argv).remain[0] : './';
 
-Promise.all([checkNetWorkPort()]).then(function() {
-  app.use( '/', express.static( staticPath , { setHeaders: headerOption }));
-  app.listen( defalutPort );
-  console.log(chalk['green'](`\n  http://127.0.0.1:${ defalutPort } / path:${ staticPath } \n`));
-});
+checkNetWorkPort().then(function () {
+  app.use('/', express.static(staticPath, { setHeaders: headerOption }));
+  app.listen(defalutPort);
+  console.log(chalk['green'](`\n  http://127.0.0.1:${defalutPort} / path:${staticPath} \n`));
+})
+  .catch(e => {
+    console.log(e);
+  });
 
 
