32a91f2f9116d8d95b5d8364d9c97ec8ece5e854
L1Yuu <i@l1yu.com>
2020-04-08 17:44:58 +0800

    fix(guide): guide page

--- desktop/index.js
+++ desktop/index.js
@@ -2,7 +2,7 @@
  * @Author: Blue 
  * @Date: 2019-09-06 17:35:22 
  * @Last Modified by: Blue
- * @Last Modified time: 2019-09-06 17:46:35
+ * @Last Modified time: 2020-03-28 19:36:27
  */
 
 const {app, BrowserWindow } = require('electron');
@@ -17,6 +17,16 @@ function createWindow() {
   })
   win.loadFile('index.html');
   win.webContents.openDevTools();
+  
+  createWindow()
+  // 隐藏菜单栏
+  const { Menu } = require('electron');
+  Menu.setApplicationMenu(null);
+  // hide menu for Mac 
+  if (process.platform !== 'darwin') {
+    app.dock.hide();
+  }
+
 }
 
 app.on('ready', createWindow);
