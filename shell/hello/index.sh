8fd979f1e0b3aa5c495b872a3089fab383cb6e6a
L1Yuu <i@l1yu.com>
2020-05-09 11:42:29 +0800

    feat: shell

---
+++ shell/hello/index.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+read -p "Please input your firstName:" firstName
+
+read -p "Please input your firstName:" lastName
+
+echo -e "\n Your Full Name is: $firstName $lastName"
