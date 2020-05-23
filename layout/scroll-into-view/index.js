8e7cd41e76c366b8c7fd379ae200ec680547581c
L1Yuu <i@l1yu.com>
2020-05-23 17:32:00 +0800

    feat:scroll-into-view

---
+++ layout/scroll-into-view/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: Blue 
+ * @Date: 2020-05-23 15:40:41 
+ * @Last Modified by: Blue
+ * @Last Modified time: 2020-05-23 15:47:56
+ */
+
+
+
+document.querySelector('.master').addEventListener('click', function() {
+  this.scrollIntoView({
+    behavior: 'smooth',
+    block: "center"
+  })
+})
+ 
+
