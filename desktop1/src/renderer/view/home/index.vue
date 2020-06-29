6afc8e4dea95aacd2c115799bce3874a2106ecdb
L1Yuu <i@l1yu.com>
2020-06-29 17:45:28 +0800

    fix:优化

---
+++ desktop1/src/renderer/view/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <countDown></countDown>
+  </div>
+</template>
+
+<script>
+
+import countDown from '@/components/countDown';
+
+
+export default {
+  components: {
+    countDown
+  }
+}
+</script>
+
+<style>
+
+</style>
