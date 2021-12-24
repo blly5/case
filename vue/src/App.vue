de74e93d45dfded9c8a6a7a76c511a3c17b0aa4f
李宇 <yu1.li@zhangmen.com>
2021-12-24 09:42:29 +0800

    feat: react

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,30 @@
-<script setup lang="ts">
-
-</script>
 
 <template>
+  <tree :data="treeData" />
 </template>
+
+<script setup lang="ts">
+  import { ref, watch } from "Vue";
+  import tree from './components/tree.vue'; 
+  const treeData = ref([
+    {
+      label: 'small',
+    }
+  ]);
+
+  watch(treeData, (val) => {
+    console.log(val);
+  })
+
+
+
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ vue/src/components/Tree.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-for="(item, index) in data" :key="index">
+    <div>{{ item.label }}</div>
+    <button @click="add(item)">+</button>
+    <div style="padding-left: 20px;" v-if="item.children" >
+      <Tree :data="item.children" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineComponent, onMounted } from "vue";
+const props = defineProps({
+  data: Array
+});
+
+const add = (item) => {
+    item.push({
+      label: '123'
+    })
+}
+
+</script>
+
+<style>
+</style>
