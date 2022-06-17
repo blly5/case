3575c79947cd92f9e6dbb48b85f2b2d5d82d105f
李宇 <yu1.li@zhangmen.com>
2022-06-17 17:42:37 +0800

    fix: add vue ssr

--- ssr/components/Tutorial.vue
+++ ssr/components/Tutorial.vue
@@ -1,12 +1,25 @@
 <!-- Please remove this file from your project -->
 <template>
   <div>
-    
+    {{ s }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NuxtTutorial'
+  name: 'NuxtTutorial',
+  data(){
+    return {
+      s: null,
+    }
+  },
+  asyncData() {
+    let s = [123, 'abc'];
+    return { s };
+  },
+  mounted() {
+    console.log('123', this.s)
+
+  }
 }
 </script>
---
+++ ssr/pages/_id/index.vue
@@ -0,0 +1,25 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <a-select v-model="value">
+      <a-select-option v-for="(item, index) in result" :key="index">{{ item.quote }}</a-select-option>
+    </a-select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+  data(){
+    return {
+      result: [],
+      value: null,
+    }
+  },
+  async asyncData() {
+    let result = await fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/fd9c289c2ee41a844cfbe99ba531aca3b483bf42/quotes.json');
+    let text =  await result.text();
+    return { result: JSON.parse(text) }
+  },
+}
+</script>
