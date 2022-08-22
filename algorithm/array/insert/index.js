a4ebe2072591309a8ec7d3fc24a59c923bee4945
李宇 <yu1.li@zhangmen.com>
2022-08-22 18:12:29 +0800

    fix: remake sort

--- algorithm/array/insert/index.js
+++ algorithm/array/insert/index.js
@@ -5,4 +5,19 @@
  * @Last Modified time: 2019-12-06 15:50:09
  */
 
+const arr = [21, 10, 2, 3, 100, 99, 22];
 
+
+function insertionSort(arr) {
+  arr.forEach( (current, index)=> {
+    let preIndex = index - 1;
+    while(preIndex >= 0 && arr[preIndex] > current) {
+      arr[preIndex + 1] = arr[preIndex];
+      preIndex--;
+    }
+    arr[preIndex + 1] = current;
+  })
+  return arr;
+};
+
+console.log(insertionSort(arr));
